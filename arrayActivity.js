1d271657616b8b245b1c0b38dc4019c41415f040
Your Name <you@example.com>
2024-10-20 08:31:38 +0800

    intial commit

---
+++ arrayActivity.js
@@ -0,0 +1,10 @@
+//favsinger
+let favSinger = ["theKidLaroi","PostMalone","BrunoMars"];
+console.log("My First Fav Singer is ", favSinger[0])
+
+let favNumber = ["06","18","21","24"];
+let mixedArr = ["string",["otherArray"],123,true];
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
